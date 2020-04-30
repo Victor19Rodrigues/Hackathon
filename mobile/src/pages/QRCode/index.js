c0fba2b6fe08e208afdb0eab2589c4d45a8f2e16
Victor Rodrigues <victor.rodrigues@somospi.com.br>
2020-04-30 20:22:16 -0300

    Added initial files for Home and QRCode pages

---
+++ mobile/src/pages/QRCode/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+// import { Container } from './styles';
+
+export default function QRCode() {
+  return (
+    <View >
+      <Text>QRCode</Text>
+    </View>
+  );
+}
